a237af3fb064761682f4f03c8df2eea3771f3e16
Vitória Moura <mouraanav@gmail.com>
2021-03-31 18:14:29 -0300

    Set functions AttackScore and SpecialAttackScore

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 new Vue({
     el: '#app',
     data: {
-        hello: 'Olá'
+        victoria: true,
+        playerScore: null,
+        monsterScore: null,
+        scoresArray: [],
+        minScore: 0,
+        maxScore: 0,
+        scoreLifePlayer: 100,
+        scoreLifeMonster: 100
     },
-    computed: {},
-    methods: {}
+    methods: {
+        setAttackScore() {
+            this.playerScore = Math.floor(Math.random() * 10) + 1;
+            this.monsterScore = Math.floor(Math.random() * 10) + 1;
+            this.scoresArray.push(this.playerScore, this.monsterScore)
+            // console.log(this.scoresArray)
+
+            
+            this.minScore = Math.min.apply(null, this.scoresArray)
+            this.maxScore = Math.max.apply(null, this.scoresArray)
+
+            this.scoresArray = []
+
+            this.scoreLifePlayer = (this.scoreLifePlayer - this.maxScore)
+            this.scoreLifeMonster = (this.scoreLifeMonster - this.minScore)
+
+            console.log(this.maxScore, this.minScore)
+        },
+        setSpecialAttackScore() {
+            this.playerScore = Math.floor(Math.random() * 10) + 1;
+            this.monsterScore = Math.floor(Math.random() * 10) + 1;
+            this.scoresArray.push(this.playerScore, this.monsterScore)
+
+            
+            this.minScore = Math.min.apply(null, this.scoresArray)
+            this.maxScore = Math.max.apply(null, this.scoresArray)
+
+            this.scoresArray = []
+
+            this.scoreLifePlayer = (this.scoreLifePlayer - this.minScore)
+            this.scoreLifeMonster = (this.scoreLifeMonster - this.maxScore)
+
+            console.log(this.maxScore, this.minScore)
+        },
+    },
 })
